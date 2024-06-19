218ddd21899d98f1782d3bd2caf55f9f99790908
asv-spb <asvdevpro@gmail.com>
2024-06-19 15:49:47 +0300

    Auto-commit

--- 7_ollama-install.sh
+++ 7_ollama-install.sh
@@ -9,7 +9,7 @@ curl -fsSL https://ollama.com/install.sh | sh
 docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_API_BASE_URL=http://127.0.0.1:11434/api --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 
 # fabric & pbpaste install
-sudo apt-get install xclip pipx -y
+sudo apt-get install xclip pipx fmpeg -y
 cd Dev
 git clone git@github.com:danielmiessler/fabric.git
 cd fabric/
