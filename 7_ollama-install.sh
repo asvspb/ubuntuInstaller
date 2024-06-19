5762539a263acd35ed5369864312f034311a76a8
asv-spb <asvdevpro@gmail.com>
2024-06-19 17:07:04 +0300

    Auto-commit

--- 7_ollama-install.sh
+++ 7_ollama-install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 echo "                                                              "
-echo "Устанавливаем ollama & fabric & pbpaste"
+echo "Устанавливаем ollama & fabric & pbpaste && shell-gpt"
 echo "--------------------------------------------------------------"
+
+# Define the directory
+dir="~/Dev"
+
+# Define the file path for shell-gpt
+file=~/.config/shell_gpt/.sgptrc
+
+# Check if the directory exists
+if [ ! -d "$dir" ]; then
+  # Create the directory
+  mkdir -p "$dir"
+  echo "Directory $dir was created."
+else
+  echo "Directory $dir already exists."
+fi
+
 # установщик сервера и окружения, включая CUDA
 curl -fsSL https://ollama.com/install.sh | sh
 
@@ -11,15 +27,15 @@ docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_API_BASE_
 # install fabric & pbpaste & shell-gpt 
 sudo apt-get install xclip pipx fmpeg -y
 pip install shell-gpt litellm
+
+
 cd Dev
 git clone git@github.com:danielmiessler/fabric.git
+
 cd fabric/
 pipx install .
 pipx ensurepath
 pipx completions
-
-# Define the file path for shell-gpt
-file=~/.config/shell_gpt/.sgptrc
 
 # Define the lines to append
 lines="CHAT_CACHE_PATH=/tmp/chat_cache\nCACHE_PATH=/tmp/cache\nCHAT_CACHE_LENGTH=100\nCACHE_LENGTH=100\nREQUEST_TIMEOUT=60\nDEFAULT_MODEL=ollama/llama3\nDEFAULT_COLOR=magenta\nROLE_STORAGE_PATH=/home/asv-spb/.config/shell_gpt/roles\nDEFAULT_EXECUTE_SHELL_CMD=false\nDISABLE_STREAMING=false\nCODE_THEME=dracula\nOPENAI_FUNCTIONS_PATH=/home/asv-spb/.config/shell_gpt/functions\nOPENAI_USE_FUNCTIONS=true\nSHOW_FUNCTIONS_OUTPUT=false\nAPI_BASE_URL=default\nPRETTIFY_MARKDOWN=true\nUSE_LITELLM=true\nOPENAI_API_KEY=abcd"
